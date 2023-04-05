75763ba8f6f87b0badeb213d50c95853112566ca
zenith1995 <versatile.skydev@gmail.com>
2023-04-05 19:48:07 -0400

    route

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,18 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer, Responder};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn index() -> impl Responder {
+    "Hello, world"
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+        App::new().service(
+            web::scope("/app")
+                .route("/index.html", web::get().to(index)),
+        )
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1",8080))?
     .run()
-    .await
+    .await    
 }
