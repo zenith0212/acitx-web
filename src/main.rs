c2be5a841ec9305ce4a3bdd29de882c25eeff4c1
zenith1995 <versatile.skydev@gmail.com>
2023-04-05 21:20:31 -0400

    shared mutuable variable

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{web, App, HttpServer};
+use std::sync::Mutex;
 
-struct Appstore {
-    app_name: String,
+struct Appstatewithcounter {
+    counter: Mutex<i32>,
 }
 
-async fn index(data: web::Data<Appstore>) -> String {
-    let app_name = &data.app_name;
-    format!("Hello {app_name} !")
+async fn index(data: web::Data<Appstatewithcounter>) -> String {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+    format!("Request number: {counter}")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let counter =  web::Data::new(Appstatewithcounter {
+        counter: Mutex::new(0)
+    });
+    HttpServer::new(move || {
         App::new()
-        .app_data(web::Data::new( Appstore {
-            app_name: String::from("Actix web")
-        }))
-        .service(
-            web::scope("/app")
-                .route("/index.html", web::get().to(index)),
-        )
+        .app_data(counter.clone())
+        .route("/", web::get().to(index))
     })
     .bind(("127.0.0.1",8080))?
     .run()
