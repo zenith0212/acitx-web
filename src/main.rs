32f21e11a114d5776f810c3f01a6b0acab134d98
zenith1995 <versatile.skydev@gmail.com>
2023-04-05 21:10:50 -0400

    state

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use actix_web::{web, App, HttpServer, Responder};
 
-async fn index() -> impl Responder {
-    "Hello, world"
+struct Appstore {
+    app_name: String,
+}
+
+async fn index(data: web::Data<Appstore>) -> String {
+    let app_name = &data.app_name;
+    format!("Hello {app_name} !")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(
+        App::new()
+        .app_data(web::Data::new( Appstore {
+            app_name: String::from("Actix web")
+        }))
+        .service(
             web::scope("/app")
                 .route("/index.html", web::get().to(index)),
         )
